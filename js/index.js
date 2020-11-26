4c17011e73f08ac9c9e42ab278c3254aeadf06ca
fernancumez <fernancumez@gmail.com>
2020-11-26 10:45:18 -0600

    Update components

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
-function handleSubmit(evt) {
-  let nombre = document.getElementById("nombre").value;
-
+const handleSubmit = (evt) => {
   evt.preventDefault();
-  alert(nombre);
-}
+  const formValues = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+  // Destructuring
+  const { firstName, lastName, email, password } = formValues;
+
+  // Create a new Object Product
+  const usuario = new User(firstName, lastName, email, password);
+
+  // Save user
+  const interfaz = new Interface();
+  interfaz.addUser(usuario);
+  interfaz.showMessage("Usuario agregado correctamente", "success");
+  interfaz.resetForm();
+};
+
+const handleDelete = (evt) => {
+  const confirm = window.confirm("Deseas eliminar este usuario?");
+
+  if (confirm) {
+    const interfaz = new Interface();
+    interfaz.deleteUser(evt.target);
+    interfaz.showMessage("Usuario eliminado correctamente", "danger");
+    evt.preventDefault();
+  }
+};
